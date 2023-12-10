e9d70a78961de5d07cd7a2b9c2ee3d502a81e78a
Yesmar A. Grullon <grullon.y@live.com>
2023-12-10 14:54:26 -0500

    click unmute function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,13 @@
 window.onload = (event) => {
-
+    document.querySelector("#videoWithJs").addEventListener("click",(event) =>{
+        if( $("#videoWithJs").prop('muted') ) {
+            $("#videoWithJs").prop('muted', false);
+            $(this).text('Mute');
+        // or toggle class, style it with a volume icon sprite, change background-position
+        } else {
+            $("#videoWithJs").prop('muted', true);
+            $(this).text('Unmute');
+        }
+    });
 
 };
